b932172294aebf3f9f8eab325aa790ae19114d4e
Vishal Pandey <vishalpandey49535@gmail.com>
2026-07-24 19:35:17 +0530

    done

--- api-gateway/src/app.js
+++ api-gateway/src/app.js
@@ -4,6 +4,10 @@ const healthRoutes = require("./routes/health.routes");
 
 const app = express();
 
+// Middleware
+app.use(express.json());
+
+// Routes
 app.use("/api/v1", healthRoutes);
 
 module.exports = app;
--- api-gateway/src/server.js
+++ api-gateway/src/server.js
@@ -4,7 +4,7 @@ dotenv.config();
 
 const app = require("./app");
 
-const PORT = process.env.PORT;
+const PORT = process.env.PORT || 3000;
 
 app.listen(PORT, () => {
     console.log(`API Gateway is running on port ${PORT}`);
