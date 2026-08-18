e8824321f00669ab51c37ec06fd1e3b83233ee54
Vishal Pandey <vishalpandey49535@gmail.com>
2026-08-18 10:47:58 +0530

    wire redis in user-service

--- services/user-service/src/server.js
+++ services/user-service/src/server.js
@@ -1,9 +1,11 @@
 const app = require("./app");
 const { PORT } = require("./config/envConfig");
 const connectDB = require("./config/dbConfig");
+const { connectRedis } = require("./config/redisConfig");
 
 const start = async () => {
     await connectDB();
+    await connectRedis();
     app.listen(PORT, () => {
         console.log(`User Service running on port ${PORT}`);
     });
